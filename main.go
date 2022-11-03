6e0c176a95f3059c5869aedd6814bc3abe4f06f8
Nguyen Thanh Dat <nguyenthanhdat@cryptopie-labo.com>
2022-11-03 09:46:39 +0700

    handle panic example

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rs/zerolog"
 	"os"
 	"reflect"
+	"runtime/debug"
 	"time"
 )
 
@@ -18,7 +19,8 @@ func init() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	w := zerolog.ConsoleWriter{
 		Out:        os.Stderr,
-		TimeFormat: time.RFC3339}
+		TimeFormat: time.RFC3339,
+	}
 	logger = zerolog.New(w).With().Timestamp().Caller().Logger()
 }
 
@@ -37,28 +39,39 @@ func dbRedis() *dbRedisStore {
 	}
 }
 
-func (d *dbRedisStore) rdGet(key string) (string, error) {
-	return d.dbRedis.Get(ctx, key).Result()
+func (d *dbRedisStore) rdGet(key string) (result string, err error) {
+	result, err = d.dbRedis.Get(ctx, key).Result()
+	return
 }
 
 func main() {
 	logger.Info().Msg("Start...")
-	go bgTask()
+	go run()
 
 	select {}
 }
 
-func bgTask() {
+func run() {
+	rdb := dbRedis()
 	ticker := time.NewTicker(1 * time.Second)
 	for _ = range ticker.C {
-		test()
+		test(rdb)
+		logger.Info().Str("test", "5s").Msg("Next time after")
 		time.Sleep(5 * time.Second)
 	}
 }
 
-func test() {
-	rdb := dbRedis()
+func test(rdb *dbRedisStore) {
+	defer func() {
+		if r := recover(); r != nil {
+			err, _ := r.(error)
+			logger.Err(err).Str("debug", string(debug.Stack())).Msg("Error")
+		}
+	}()
+	a := divide(1, 0)
+	fmt.Println(a)
 	val, err := rdb.rdGet("pair_setting:coin06:usdt")
+
 	if err != nil {
 		logger.Err(err).Msg("error redis")
 	}
@@ -70,4 +83,8 @@ func test() {
 	fmt.Println(reflect.TypeOf(out))
 	fmt.Println(out["max_amount"])
 	logger.Info().Msg(val)
+}
+
+func divide(a, b int) int {
+	return a / b
 }
